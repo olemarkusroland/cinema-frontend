7bbea4045f2e261bfe10c31de54044f4d9fdfd85
Ole Markus Roland <ole.markus.roland@no.experis.com>
2024-05-22 15:05:38 +0200

    Changed date format on ScreeningCard

--- src/components/screening-card/ScreeningCard.jsx
+++ src/components/screening-card/ScreeningCard.jsx
@@ -1,15 +1,16 @@
 import './ScreeningCard.css';
-
 import PropTypes from 'prop-types';
-
 import { Link } from 'react-router-dom';
+import dayjs from 'dayjs';
 
-export const ScreeningCard = ({ screening, isClickable=true, includeDate=true }) => {
+export const ScreeningCard = ({ screening, isClickable = true, includeDate = true }) => {
   const { id, date, time, auditorium } = screening;
 
+  const formattedDate = dayjs(date).format('dddd DD.MM');
+
   const content = (
-    <div className={`place-time ${isClickable ? 'place-time--clickable' : 'place-time--non-clickable'} screening-card-place-time `}>
-      {includeDate && <p className={`date ${isClickable ? 'date--clickable' : 'date--non-clickable'}`}>{date}</p>}
+    <div className={`place-time ${isClickable ? 'place-time--clickable' : 'place-time--non-clickable'} screening-card-place-time`}>
+      {includeDate && <p className={`date ${isClickable ? 'date--clickable' : 'date--non-clickable'}`}>{formattedDate}</p>}
       <p className='time'>{time}</p>
       <p className='auditorium'>{auditorium}</p>
     </div>
