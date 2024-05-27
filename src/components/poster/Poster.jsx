23c7347f470107122fc8d8dce721a2e3a23ef6b2
Ole Markus Roland <ole.markus.roland@no.experis.com>
2024-05-27 13:37:41 +0200

    Updated TicketCard to have dynamic poster, and no text wrap for title.

--- src/components/poster/Poster.jsx
+++ src/components/poster/Poster.jsx
@@ -1,11 +1,12 @@
+import './Poster.css'
 import poster from '../../assets/pusur-filmen.webp';
 
 export const Poster = ({ movie, className }) => {
     return (
-        <div>
+        <div className="poster-container">
             <img
                 className={className}
-                src={poster}
+                src={movie.poster}
                 alt={`${movie.title} Poster`}
             />
         </div>
