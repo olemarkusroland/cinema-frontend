aef1013e5842913eaf39e37456ffc80db506c8e9
Ole Markus Roland <ole.markus.roland@no.experis.com>
2024-05-28 09:49:45 +0200

    Updated Movie page to dynamicaly use information from database.

--- src/components/movie/Movie.jsx
+++ src/components/movie/Movie.jsx
@@ -4,32 +4,51 @@ import { ScreeningCard } from '../screening-card/ScreeningCard';
 import { ScreeningContext } from '../../context/ScreeningContext';
 import { useParams } from 'react-router-dom';
 import { MovieContext } from '../../context/MovieContext';
-import { Poster } from '../poster/Poster';
 
 export const Movie = () => {
     const { id } = useParams();
     const { movies } = useContext(MovieContext);
     const { screenings } = useContext(ScreeningContext);
 
-    const movie = movies.find(m => m.id === parseInt(id));
-    const filteredScreenings = screenings.filter(s => s.movieId === parseInt(id));
+    const movie = movies.find(m => m.imdbID === id);
+    
+    const filteredScreenings = screenings.filter(s => s.movieId === id);
 
     if (!movie) {
         return <div>Movie with id {id} not found</div>;
     }
 
+    const movieDetails = [
+        { label: 'Released', value: movie.released },
+        { label: 'Runtime', value: movie.runtime },
+        { label: 'Genre', value: movie.genre },
+        { label: 'Rated', value: movie.rated },
+        { label: 'Director', value: movie.director },
+        { label: 'Writer', value: movie.writer },
+        { label: 'Actors', value: movie.actors },
+        { label: 'Language', value: movie.language },
+        { label: 'Country', value: movie.country },
+        { label: 'Awards', value: movie.awards },
+        { label: 'Metascore', value: movie.metascore },
+        { label: 'Box Office', value: movie.boxOffice },
+        { label: 'Production', value: movie.production },
+        { label: 'Website', value: movie.website },
+        { label: 'DVD Release', value: movie.dvd },
+        { label: 'IMDb Rating', value: movie.imdbRating },
+        { label: 'IMDb Votes', value: movie.imdbVotes },
+    ];
+
     return (
         <div className='movie'>
             <div className='movie-header'>
-                <Poster className="movie-poster" movie={movie} />
-                <div className='movie-details'>
-                    <h1 className='movie-title'>{movie.name}</h1>
-                    <p className='movie-description'>
-                        Pusur Filmen is a heartwarming adventure that follows the mischievous yet lovable cat Pusur. 
-                        Join Pusur as he navigates through hilarious and exciting escapades, meeting a cast of quirky 
-                        characters along the way. From epic chases to touching moments of friendship, this film promises 
-                        to entertain audiences of all ages with its charm and humor.
-                    </p>
+                <img
+                    className="movie-poster"
+                    src={movie.poster}
+                    alt={`${movie.title} Poster`}
+                />
+                <div className='movie-title-description-container'>
+                    <h1 className='movie-title'>{movie.title}</h1>
+                    <p className='movie-description'>{movie.plot}</p>
                 </div>
             </div>
             <div className='place-times'>
@@ -38,7 +57,17 @@ export const Movie = () => {
                         <ScreeningCard key={index} screening={screening} className="movie-place-time" />
                     ))
                 ) : (
-                    <p>No screenings available</p>
+                    <p>There are currently no screenings available</p>
+                )}
+            </div>
+            <div className='movie-details'>
+                {movieDetails.map((detail, index) =>
+                    detail.value && detail.value !== "N/A" ? (
+                        <div key={index} className='movie-detail'>
+                            <span className='detail-label'>{detail.label}: </span>
+                            <span className='detail-value'>{detail.value}</span>
+                        </div>
+                    ) : null
                 )}
             </div>
         </div>
--- src/components/poster/Poster.jsx
+++ src/components/poster/Poster.jsx
@@ -1,5 +1,4 @@
 import './Poster.css'
-import poster from '../../assets/pusur-filmen.webp';
 
 export const Poster = ({ movie, className }) => {
     return (
